d317d28b601984c314776dc7fe785bc9416834bd
Jason Pollard <jpollard91@msn.com>
2021-08-08 16:40:25 -0500

    Add Validation and tests

---
+++ src/test/java/com/example/cnscatalogservice/web/BookControllerIntegrationTests.java
@@ -0,0 +1,30 @@
+package com.example.cnscatalogservice.web;
+
+import com.example.cnscatalogservice.domain.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.Year;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BookControllerIntegrationTests {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void whenPostRequestThenBookCreated() {
+        Book expectedBook = new Book("1231231231", "Title", "Author", Year.of(1991), 9.90);
+        ResponseEntity<Book> response = restTemplate.postForEntity("/books", expectedBook, Book.class);
+        assertThat(response.getStatusCode())
+                .isEqualTo(HttpStatus.CREATED);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().getIsbn()).isEqualTo(expectedBook.getIsbn());
+    }
+}
---
+++ src/test/java/com/example/cnscatalogservice/web/BookControllerMvcTests.java
@@ -0,0 +1,32 @@
+package com.example.cnscatalogservice.web;
+
+import com.example.cnscatalogservice.domain.BookNotFoundException;
+import com.example.cnscatalogservice.domain.BookService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@WebMvcTest(BookController.class)
+public class BookControllerMvcTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BookService bookService;
+
+    @Test
+    void whenReadingNotExistingBookThenShouldReturn404() throws Exception {
+        String isbn = "73737313940";
+        given(bookService.viewBookDetails(isbn)).willThrow(BookNotFoundException.class);
+        mockMvc
+                .perform(get("/books/" + isbn))
+                .andExpect(status().isNotFound());
+    }
+}
