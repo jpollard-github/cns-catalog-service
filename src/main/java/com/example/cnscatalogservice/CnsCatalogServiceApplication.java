8938620b501ba15e654357053908b3ee3e9e44c9
Jason Pollard <jpollard91@msn.com>
2021-08-08 11:34:36 -0500

    first commit

---
+++ src/main/java/com/example/cnscatalogservice/CnsCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.cnscatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnsCatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnsCatalogServiceApplication.class, args);
+    }
+
+}
