98a5ecfe616299ad3658a26114bb26af5e506567
Jason Pollard <jpollard91@msn.com>
2021-08-09 14:34:11 -0500

    Configuration

--- src/main/java/com/example/cnscatalogservice/CnsCatalogServiceApplication.java
+++ src/main/java/com/example/cnscatalogservice/CnsCatalogServiceApplication.java
@@ -2,12 +2,26 @@ package com.example.cnscatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerRequest;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.web.servlet.function.RequestPredicates.GET;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
+import static org.springframework.web.servlet.function.ServerResponse.ok;
 
 @SpringBootApplication
+@EnableConfigurationProperties(PolarProperties.class)
 public class CnsCatalogServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CnsCatalogServiceApplication.class, args);
     }
 
+    @Bean
+    RouterFunction<ServerResponse> routes(PolarProperties properties) {
+        return route(GET("/"), request -> ok().body(properties.getGreeting()));
+    }
 }
